05b8c8be6b40fced7053c4424a5d19a85132d544
Hari Ramanathan <hp.ramanathan@gmail.com>
2023-04-17 17:53:32 +0100

    4. Minor edits

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css'
 import React, { useState } from "react"
 
-const API_URL = `https://dictionaryapi.com/api/v3/references/collegiate/json/curfew?key=1ae97b82-bd14-46a8-a87d-e1082c1300b4`
+/* const API_URL = `https://dictionaryapi.com/api/v3/references/collegiate/json/curfew?key=1ae97b82-bd14-46a8-a87d-e1082c1300b4`
 
 fetch(API_URL)
     .then((response) => {
@@ -9,45 +9,39 @@ fetch(API_URL)
 })
     .then((response) => {
         console.log(response)
-})
+}) */
 
 function App() {
-
-  const [wordSearch, setWordSearch] = useState('Word...')
-
   return (
-    <div className="App">
-      <header className="App-header">
-        <>
-          <h1 className="App-h1">What's the origin of...</h1>
-            <form>
-              <label>Type word here:
-                <input 
-                  value={wordSearch}
-                  onChange={e => setWordSearch(e.target.value)}></input>
-              </label>
-              {wordSearch !== '' && <p>You want to search for: {wordSearch}.</p>}
-              <button 
-                type="submit"
-                onClick={alert}>SEARCH</button>
-            </form>
-        </>
-      </header>
+    <div id="recentSearches">
+      <PastSearchList />
     </div>
-  );
+  )
 }
 
+function PastSearchList() {
+  const [wordSearch, setWordSearch] = useState('')
+
+  return (
+      <div className="pastSearchList">
+        <header className="pastSearchList-header">
+            <h1 className="pastSearchList-h1">What's the origin of...</h1>
+              <form>
+                  <input
+                    name="myInput"
+                    value={wordSearch}
+                    onChange={e => setWordSearch(e.target.value)}>
+                  </input>
+                
+                <button
+                  id="homeSearchButton" 
+                  type="submit"
+                  onClick={alert}>SEARCH</button>
+              </form>
+        </header>
+        {wordSearch !== '' && <p>You want to search for: {wordSearch}.</p>}
+    </div>
+    )
+  }
+
 export default App;
-
-
-
-
-/**
- * Doc comment
- */
-
-// Who wants this
-
-/*
- * Block comment
- */
