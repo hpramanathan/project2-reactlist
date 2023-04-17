baf29392eaf1cbea9a0989f994bbe6ed0320ef7a
Hari Ramanathan <hp.ramanathan@gmail.com>
2023-04-17 16:01:21 +0100

    3. Wireframe, User Stories & Initial App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import React, { useState } from "react"
 
 const API_URL = `https://dictionaryapi.com/api/v3/references/collegiate/json/curfew?key=1ae97b82-bd14-46a8-a87d-e1082c1300b4`
 
@@ -12,12 +12,25 @@ fetch(API_URL)
 })
 
 function App() {
+
+  const [wordSearch, setWordSearch] = useState('Word...')
+
   return (
     <div className="App">
       <header className="App-header">
         <>
-          <h1>What's the origin of...</h1>
-
+          <h1 className="App-h1">What's the origin of...</h1>
+            <form>
+              <label>Type word here:
+                <input 
+                  value={wordSearch}
+                  onChange={e => setWordSearch(e.target.value)}></input>
+              </label>
+              {wordSearch !== '' && <p>You want to search for: {wordSearch}.</p>}
+              <button 
+                type="submit"
+                onClick={alert}>SEARCH</button>
+            </form>
         </>
       </header>
     </div>
@@ -25,3 +38,16 @@ function App() {
 }
 
 export default App;
+
+
+
+
+/**
+ * Doc comment
+ */
+
+// Who wants this
+
+/*
+ * Block comment
+ */
