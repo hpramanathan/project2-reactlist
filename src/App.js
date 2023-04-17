108d3943420e087d71d45925764c69bd57161d41
Hari Ramanathan <hp.ramanathan@gmail.com>
2023-04-17 11:44:12 +0100

    2. Checking to make sure I can fetch API data from M-W

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
 
+const API_URL = `https://dictionaryapi.com/api/v3/references/collegiate/json/curfew?key=1ae97b82-bd14-46a8-a87d-e1082c1300b4`
+
+fetch(API_URL)
+    .then((response) => {
+        return response.json()
+})
+    .then((response) => {
+        console.log(response)
+})
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <>
-          <h1>Project 2</h1>
+          <h1>What's the origin of...</h1>
+
         </>
       </header>
     </div>
